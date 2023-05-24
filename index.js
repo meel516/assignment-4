5e6e161b165c3d28e954dd3abf53c14f0fbf1ce0
meel516 <md.saleem516@gmail.com>
2023-05-24 16:18:45 +0530

    created add

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
 let express=require('express')
+const bodyParser = require('body-parser');
 let app=express()
+app.use(bodyParser.json()); // for parsing JSON data
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing URL-encoded data
+
+
 app.get('/',(req,res)=>res.end('welcome'))
 app.listen(4000,(re1,res)=>console.log('listening on port 4000'))
+app.post('/add',(req,res)=>{
+    console.log(req.body)
+    let num1=req.body.num1
+    let num2=req.body.num2
+    res.status(201).json(
+       { status: "success",
+        message: "the sum of given two numbers",
+        sum: num1+num2 }
+       
+       
+    )
+})
